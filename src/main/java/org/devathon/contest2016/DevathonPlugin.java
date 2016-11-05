aa0b98eec73f5d558e5a2c0b56246287e5317a8e
Mintcraftian <mintygaming36@gmail.com>
2016-11-05 14:56:26 +0000

    Test

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -4,14 +4,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class DevathonPlugin extends JavaPlugin {
 
-    @Override
     public void onEnable() {
-        // put your enable code here
+
     }
 
-    @Override
     public void onDisable() {
-        // put your disable code here
     }
 }
 
